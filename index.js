32c1ea98740acf45011d869f612f56f4d1a9ca1b
moxica80 <nickmegasoft@gmail.com>
2025-10-02 19:12:32 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var btn_chat = document.querySelector("#chat_btn");
+var btn_my_spot = document.querySelector("#my_spot_btn");
+
+btn_chat.addEventListener("click", ()=>{  
+    var a = document.createElement("a");      
+    a.href = "https://drive.google.com/file/d/170rFcPG1xUUk3VfdryyF35_1-i-s1Z8L/view?usp=drive_link"
+    a.referrerPolicy = "origin";
+    a.setAttribute("download", "chat-app.apk");    
+    a.click();
+    a.remove();    
+});
+
+// btn_my_spot.addEventListener("click", ()=>{  
+//     var a = document.createElement("a");      
+//     a.href = "https://drive.google.com/file/d/1U6DSzDo6jjUCRcIG3LpMvBFWb8qYIp2v/view?usp=drive_link"
+//     a.referrerPolicy = "origin";
+//     a.setAttribute("download", "chat-app.apk");    
+//     a.click();
+//     a.remove();    
+// });
+
