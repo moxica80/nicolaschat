3629fffe6b396f11c8487d10922548911b24aa34
moxica80 <nickmegasoft@gmail.com>
2026-01-12 20:13:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ btn_chat.addEventListener("click", ()=>{
     var a = document.createElement("a");      
     a.href = "https://drive.google.com/file/d/170rFcPG1xUUk3VfdryyF35_1-i-s1Z8L/view?usp=drive_link"
     a.referrerPolicy = "origin";
-    a.setAttribute("download", "chat-app.apk");    
+    a.setAttribute("download", "chat-app.png");    
     a.click();
     a.remove();    
 });
@@ -14,7 +14,7 @@ btn_chat.addEventListener("click", ()=>{
 //     var a = document.createElement("a");      
 //     a.href = "https://drive.google.com/file/d/1U6DSzDo6jjUCRcIG3LpMvBFWb8qYIp2v/view?usp=drive_link"
 //     a.referrerPolicy = "origin";
-//     a.setAttribute("download", "chat-app.apk");    
+//     a.setAttribute("download", "chat-app.png");    
 //     a.click();
 //     a.remove();    
 // });
