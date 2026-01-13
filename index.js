1f7e71b91f425109789dc67528031007fa86a5f1
moxica80 <nickmegasoft@gmail.com>
2026-01-13 21:14:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var btn_chat = document.querySelector("#chat_btn");
 var btn_my_spot = document.querySelector("#my_spot_btn");
 
-btn_chat.addEventListener("click", ()=>{  
-
-    btn_chat.addEventListener("click", () => {
+btn_chat.addEventListener("click", ()=>{      
     const a = document.createElement("a");
     a.href = "https://drive.google.com/uc?export=download&id=13-IjaSSxiFRXNdGLbbVTgX0lh3UUCBeb";
     a.setAttribute("download", "chat-app.png");
